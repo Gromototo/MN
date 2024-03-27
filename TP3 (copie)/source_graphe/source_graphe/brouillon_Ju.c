f02c5d4e592e1144bf37c43c449906db0120044c
Julian Mazerat <Julian.Mazerat@etu.univ-grenoble-alpes.fr>
2024-03-27 15:18:01 +0100

    Avancer sur Dijkstra

--- TP3 (copie)/source_graphe/source_graphe/brouillon_Ju.c
+++ TP3 (copie)/source_graphe/source_graphe/brouillon_Ju.c
@@ -20,15 +20,27 @@ void algo_dijkstra (pgraphe_t g, int r)
   boucle_dijkstra(sommets,g,nb_sommets);
 }
 
+sommet_t* parcours_graph_init_dijkstra(pgraphe_t g){
+
+}
+
 void boucle_dijkstra(sommet_t* sommets, pgraphe_t g, int nb_sommets){
   while (nb_sommets > 0){
-    sommet_t current = extraire_min_liste(sommets, nb_sommets);
+    sommet_t current = extraire_min_liste(&sommets, nb_sommets);
   }
 }
 
-extraire_min_liste(sommet_t* sommets, int nb_sommets){
-  min = sommets[0];
-  for (int i=0; i<nb_sommets; i++){
-    if ()
+sommet_t extraire_min_liste(int nb_sommets, sommet_t sommets[nb_sommets]){
+  int min = sommets[0].label;
+  int indice=0;
+  sommet_t current = sommets[0];
+  for (int i=1; i<nb_sommets; i++){
+    if (sommets[i].label < min){
+      min = sommets[i].label;
+      current = sommets[i];
+
+    }
   }
+  
+  return current;
 }
