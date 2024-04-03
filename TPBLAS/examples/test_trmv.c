6fdbc0d7bb44ddece1e9fe337ec8c1652986d47a
Jessy <jessysanfilippo02@outlook.fr>
2024-04-03 14:02:42 +0200

    trmv et testtrmv (fallait pas faire)

--- TPBLAS/examples/test_trmv.c
+++ TPBLAS/examples/test_trmv.c
@@ -1,2 +1,20 @@
 #include "../src/BLAS2/trmv.c"
 #include <stdio.h>
+
+void test00(){
+    float A[9] = {1.0,0.0,0.0,2.0,1.0,0.0,4.0,3.0,2.0};
+    float B[9] = {1.0,2.0,1.0,0.0,3.0,2.0,0.0,0.0,1.0};
+
+    float x[3] = {3.0,2.0,2.0};
+    mncblas_strmv(MNCblasLower, MNCblasNoTrans,MNCblasNonUnit,3,A,1,x,1);
+
+    for (int i = 0; i<3; i++){
+        printf("%f\n",x[i]);
+    }
+
+}
+
+int main(){
+    test00();
+
+}
--- TPBLAS/src/BLAS2/trmv.c
+++ TPBLAS/src/BLAS2/trmv.c
@@ -3,43 +3,33 @@
 
 void mncblas_strmv(MNCBLAS_UPLO uplo, MNCBLAS_TRANSPOSE trans, MNCBLAS_DIAG diag, int n, float* A,
 int lda, float* x, int incX){
-    int i; //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
     int offset;
-    int j = 0;
-    float currentx[n];
     if (uplo == MNCblasUpper){ // si matrice triangulaire supérieure
         offset = 0;
         float res = 0;
-        for (i = 0; i<n*n; i++){
-            res += A[i+offset]*x[j];
+        for (int j = 0; j<n*n;){
+            res += A[j+offset]*x[(j+offset)%n];
             j++;
             if (j%n == 0){
-                currentx[offset]= res;
+                x[offset]= res;
                 offset++;
                 res = 0;
-                j = offset;
             }
-        }
-        for (i = 0; i<n; i++){
-            x[i] = currentx[i];
         }
     }
     else {
-        int aug = n-1;
-       offset = 0;
+        offset = 0;
         float res = 0;
-        for (i = 0; i<n*n; i++){
-            res += A[i+offset]*x[j];
-            j++;
-            if (j%(offset+1) == 0){
-                currentx[offset] = res;
-                offset+=aug;
-                aug--;
+        for (int j = n*n-1; j>-1;){
+            res += A[j-offset]*x[(j-offset)%n];
+            if (j%n == 0){
+                x[n-1-offset]= res;
+                offset++;
                 res = 0;
-                j = 0;
             }
+            j--;
         }
-        
     }
-    
 }
+    
+    
