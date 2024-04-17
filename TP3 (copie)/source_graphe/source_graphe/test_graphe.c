bd4824106222e2a8e6c50ca4b7f10d8b629de963
Gromototo <rodlpd51@gmail.com>
2024-04-17 09:44:45 +0200

    comms fonctions

--- TP3 (copie)/source_graphe/source_graphe/test_graphe.c
+++ TP3 (copie)/source_graphe/source_graphe/test_graphe.c
@@ -29,41 +29,18 @@ int main (int argc, char **argv)
   
   printf ("nombre de sommets du graphe %d nombre arcs %d \n", nombre_sommets (g), nombre_arcs (g)) ;
   fflush (stdout) ;
-  
   ecrire_graphe (g) ;      
-
   nc = colorier_graphe (g) ;
-  
   printf ("nombre chromatique graphe = %d\n", nc) ;
 
   ecrire_graphe_colorie (g) ;
 
-  int visites[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-  int last_index = -1;
-
-  last_index = visiter(2, visites, last_index);
-  printf("deja visité ? %d \n", deja_visite(2, visites, last_index));
-  printf("VISITES : ");
-  for (int i = 0; i < 10; i++) {
-    printf("%d ", visites[i]);
-  }
-  printf("FIN VISITES\n");
-
-
   printf("degre maximal graphe %d\n", degre_maximal_graphe(g));
   printf("degre minimal grpahe %d\n", degre_minimal_graphe(g));
-  printf("graphe indepedantn %d\n", independant(g));
+  printf("graphe indepedant %d\n", independant(g));
   printf("graphe complet %d \n", complet(g));
   printf("graphe regulier %d \n", regulier(g));
 
-  psommet_t s = g;
-  while (s!=NULL) {
-    printf("sommet %d\n", s->label);
-    printf("degre sortant %d\n", degre_sortant_sommet(g, s));
-    printf("degre entrant %d\n", degre_entrant_sommet(g, s));
-
-    s = s->sommet_suivant;
-  }
 
   printf("Afficher graphe Largeur : \n");
   afficher_graphe_largeur(g, 1);
